d2d20ed061b5fe66a5009a84ea3b5d7ed0e40034
Yuno <1014893924@qq.com>
2023-04-27 16:34:56 +0800

    秒杀-一人只能购买一张优惠券（只能解决单机模式的并发问题）

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,6 +10,9 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,8 +38,12 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
 
+    @Resource
+    private IVoucherOrderService iVoucherOrderService;
+
+    private static int count = 1;
+
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         //1.查询优惠卷信息
         SeckillVoucher seckill = iSeckillVoucherService.getById(voucherId);
@@ -53,7 +60,27 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (seckill.getStock() < 1) {
             return Result.fail("秒杀券库存不足");
         }
-        //4.库存充足则扣减库存，并创建订单
+
+        /*
+        (1)一人一单，所以锁的应该是用户。只有当同一个用户并发请求时才加锁，不同的用户不用加锁。
+        (2)不能使用Long当锁，因为Long是一个对象。使用字符串当作锁，需要加上intern()，因为toString()底层返回的时new String()
+        (3)需要锁整个方法，因为需要等事务提交之后才能释放锁。而事务实在方法执行完成之后才提交的
+        注意：这种解决方案只适合单机模式
+        */
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()){
+            return iVoucherOrderService.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        Long userId = UserHolder.getUser().getId();
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if(count > 0){
+           return  Result.fail("该优惠券每人限购一张");
+        }
+        //4.未购买过并且库存充足则扣减库存，并创建订单
         //4.1 扣减库存
         boolean success = iSeckillVoucherService.update().setSql("stock = stock - 1")
                 .eq("voucher_id", voucherId)
@@ -64,10 +91,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
         //4.2 创建订单
         long orderId = redisIdWorker.nextId(BUSINESS_NAME);
-        UserDTO user = UserHolder.getUser();
         VoucherOrder order = new VoucherOrder();
         order.setId(orderId);
-        order.setUserId(user.getId());
+        order.setUserId(userId);
         order.setVoucherId(voucherId);
         save(order);
         //7.返回订单Id
