e3e0ef0e68ea8accc842917a468ec1fca81d7994
Yuno <1014893924@qq.com>
2023-04-28 11:07:21 +0800

    秒杀-分布式锁误删的解决办法

--- hm-dianping/src/main/java/com/hmdp/utils/lock/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/lock/SimpleRedisLock.java
@@ -1,9 +1,8 @@
 package com.hmdp.utils.lock;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Component;
-import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -11,6 +10,9 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private String prefix;
     private String key;
+
+    //UUID主要是用于区分在分布式模式下不同的机器，相当于服务器ID
+    private static final String ID_PREFIX = UUID.randomUUID(true).toString() + "-";
 
     public SimpleRedisLock() {
     }
@@ -23,13 +25,19 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public boolean tryLock(long timeoutSes) {
-        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(prefix + key, Thread.currentThread().getName(), timeoutSes, TimeUnit.SECONDS);
+        String threadId = ID_PREFIX + Thread.currentThread().getId() + "";
+        //不同的jvm中有可能出现相同的threadId，使用UUID区分jvm
+        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(prefix + key, threadId, timeoutSes, TimeUnit.SECONDS);
         return BooleanUtil.isTrue(lock);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(prefix + key);
-
+        String curThreadId = ID_PREFIX + Thread.currentThread().getId() + "";
+        String cacheThreadId = stringRedisTemplate.opsForValue().get(prefix + key);
+        //当jvm相同，也就是uuid相同时，并且是同一个线程，说明锁属于自己才有权力释放
+        if(curThreadId.equals(cacheThreadId)){
+            stringRedisTemplate.delete(prefix + key);
+        }
     }
 }
